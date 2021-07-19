4f64d63d88ee91c03bc19458f1a084f857a24bd2
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-07-19 16:59:14 +0200

    up

--- install_app.sh
+++ install_app.sh
@@ -2,10 +2,10 @@
 APP_DIR=$(readlink -e "${0%/*}")
 
 echo $APP_DIR
-# python3 -p /usr/bin/python3 -m venv 
+python3 -p /usr/bin/python3 -m venv 
 
-# source venv/bin/activate
-# pip install -r requirements.txt
+source venv/bin/activate
+pip install -r requirements.txt
 
 sudo cp trombi.service.template /etc/systemd/system/trombi.service
 sudo sed -i "s%{{USER}}%${USER}%" /etc/systemd/system/trombi.service
