7e6a4cfd095f639573c6e152975da04e21893522
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-09-13 17:14:29 +0200

    correction on config.json fetch on subdirectory

--- ldaptrombipy/static/app/src/main.js
+++ ldaptrombipy/static/app/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from "axios"
 // load config
-axios.get(`${process.env.BASE_URL}/config.json`).then(resp => {
+axios.get("./config.json").then(resp => {
     const app = createApp(App)
     app.config.globalProperties.$config = resp.data
     app.mount('#app')
