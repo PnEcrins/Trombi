e66b1b055448a629c65afd371834000e51766ee7
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-05-22 18:06:28 +0200

    upgrade flask 2.3

--- ldaptrombipy/app.py
+++ ldaptrombipy/app.py
@@ -1,28 +1,28 @@
+from flask.json.provider import DefaultJSONProvider
 from flask import Flask
-from flask.json import JSONEncoder
 from ldap3.utils.ciDict import CaseInsensitiveDict
 from flask_cors import CORS
 
 from ldaptrombipy import config
 
 
-class CustomJsonEncoder(JSONEncoder):
+class CustomJsonEncoder(DefaultJSONProvider):
     def default(self, obj):
         if isinstance(obj, CaseInsensitiveDict):
             return dict(obj)
-
-        return JSONEncoder.default(self, obj)
 
 
 def create_app():
     app = Flask(__name__)
     with app.app_context():
-        app.json_encoder = CustomJsonEncoder
+        app.json = CustomJsonEncoder(app)
         from ldaptrombipy.ldpap_api import blueprint
         from ldaptrombipy import errors
+
         app.register_blueprint(blueprint)
-        
+
         from ldaptrombipy.caldav_api import blueprint
+
         app.register_blueprint(blueprint, url_prefix="/caldav")
         CORS(app, supports_credentials=True)
     return app
